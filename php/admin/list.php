783af2b516d2824f06a010fd124b51f7978e6c53
Pedro Labrador <pedrolabrador3@outlook.com>
2018-02-24 01:21:48 -0430

    Update register controller

--- php/admin/list.php
+++ php/admin/list.php
@@ -24,7 +24,7 @@
         		</tr>
 				<?php
 	                $sql = "SELECT teams.teamname, tournaments.name, registers.id, registers.team_id, registers.category, registers.n_participants FROM teams, tournaments, registers WHERE teams.id = registers.team_id AND tournaments.id = registers.tournament_id";          
-	                $query = $pdo->prepare ($sql);
+ 	                $query = $pdo->prepare ($sql);
 	                $result = $query->execute ();
 
 	                while ($current = $query->fetch (PDO::FETCH_ASSOC)):
@@ -34,9 +34,9 @@
 	                    <td><?= $current['category'] ?></td>
 	                    <td><?= $current['teamname'] ?></td>
 	                    <td><?= $current['n_participants'] ?></td>
-	                    <td><a href="details.php?id=<?= $current['team_id'] ?>">Detalles</a></td>
-	                    <td><a href="update.php?id=<?= $current['id'] ?>">Editar</a></td>
-	                    <td><a href='delete.php?id=<?= $current['id'] ?>'>Borrar</a></td>
+	                    <td><a href="details.php?id=<?= $current['team_id'] ?>">Details</a></td>
+	                    <td><a href="edit.php?id=<?= $current['id'] ?>&tn=<?= $current['teamname'] ?>&to=<?= $current['name'] ?>&ca=<?= $current['category'] ?>&tid=<?= $current['team_id'] ?>">Edit</a></td>
+	                    <td><a href='delete.php?id=<?= $current['id'] ?>'>Delete</a></td>
 	                </tr>
 	            <?php endwhile ?>
                 
