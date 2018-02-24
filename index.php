a25b9843201034248b93b53001bb22bd185d72bb
Pedro Labrador <pedrolabrador3@outlook.com>
2018-02-24 19:14:43 -0430

    Validations, and routes (no css yet)

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
+<?php 
+	session_start();
+	if (isset($_SESSION['user'])) {
+        if ($_SESSION['user']['status'] == 1) {
+            header("location:/php/admin/index.php");
+        } else {
+            header("location:/php/tournament_register.php");
+        }
+    }
+?>
 <!DOCTYPE html>
 <html>
     <head>
@@ -5,6 +15,7 @@
         <title>Sport tournament system</title>
     </head>
     <body>
-        
+        <a href="/php/auth/login.php">Sign in</a>
+        <a href="/php/auth/register.php">Sign up</a>
     </body>
 </html>
