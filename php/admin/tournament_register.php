24cf4d5f9df03b1f15844a78574d9ea8d0d722ee
Pedro Labrador <pedrolabrador3@outlook.com>
2018-02-24 01:23:13 -0430

    Tournaments creation module

---
+++ php/admin/tournament_register.php
@@ -0,0 +1,46 @@
+<?php 
+    include_once '../config/config.php';
+    $pdo = Database::getConnection();
+
+    if (isset($_POST['register'])) {
+        $username = filter_input(INPUT_POST, 'name',FILTER_SANITIZE_STRING);
+        $date = filter_input(INPUT_POST, 'date',FILTER_SANITIZE_STRING);
+
+        $sql = "INSERT INTO tournaments (name, date, status) VALUES (:name, :date, :status)";
+        $query = $pdo->prepare($sql);
+        $result = $query->execute([
+            'name'   => $username,
+            'date'   => $date,
+            'status' => 1
+        ]);
+    }
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Admin panel - Create tournament</title>
+        <link rel="stylesheet" type="text/css" href="/css/style.css">
+    </head>
+    <body>
+        <div class='container'>
+            <form method='post'>
+                <fieldset class='content'>
+                    <legend>Tournament register</legend>
+                    <div class="separator">
+                        <label for='name'>Tournament name: </label>
+                        <input id='name' name='name' type='text' required>
+                    </div>
+                    <div class="separator">
+                        <label for='id'>Date: </label>
+                        <input id='date' name='date' type='date' required>
+                    </div>
+                    <div class="separator">
+                        <input type='submit' name='register' value='register'>
+                    </div>
+                </fieldset>
+            </form>
+        </div>
+    </body>
+</html>
