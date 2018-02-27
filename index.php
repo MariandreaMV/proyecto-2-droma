8df6b7cf5c06d81a2b95f1f31ae045bcc20cd6b3
MariandreaMV <mariandrea.maldonado@unet.edu.ve>
2018-02-26 21:41:15 -0400

    Improved landing page

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 	session_start();
 	if (isset($_SESSION['user'])) {
         if ($_SESSION['user']['status'] == 1) {
@@ -16,15 +16,15 @@
         <link rel="stylesheet" href="/css/main.css" />
         <title>SPORT TOURNAMENT SYSTEM</title>
     </head>
-    <body> 
+    <body>
         <div id="header">
             <nav>
                 <ul>
                     <li><a href="/php/auth/register.php">Sign up</a></li>
                     <li><a href="/php/auth/login.php">Sign in</a></li>
                 </ul>
-             </nav>   
+             </nav>
             <h1>SPORTS TOURNAMENTS</h1>
-        </div>    
+        </div>
     </body>
 </html>
